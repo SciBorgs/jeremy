8db865c11dc054d8c879382ae6f21330357b363f
Asa Paparo <asapaparo@gmail.com>
2022-12-28 20:07:04 -0600

    init

---
+++ src/main/kotlin/frc/robot/subsystems/ExampleSubsystem.kt
@@ -0,0 +1,13 @@
+package frc.robot.subsystems
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+
+class ExampleSubsystem : SubsystemBase() {
+    override fun periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    override fun simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+}
